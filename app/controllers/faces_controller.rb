8828e0c9e6f53166fae72ae1dee726b0dddb670c
Brentwood Reid <bwoodreid@gmail.com>
2015-05-18 05:44:42 -0800

    Add edit and index pages

--- app/controllers/faces_controller.rb
+++ app/controllers/faces_controller.rb
@@ -21,6 +21,28 @@ class FacesController < ApplicationController
 		end
 	end
 
+	def edit
+		@face = Face.find(params[:id])
+	end
+
+	def update
+		@face = Face.find(params[:id])
+		if @face.update_attributes(face_params)
+		  flash.now[:success] = "Face Updated!"
+			redirect_to @face
+		else
+			render 'edit'
+		end
+		# 	respond_to do |format|
+		# 		# flash.now[:success] = "face updated!"
+		# 		format.html  { redirect_to @face }
+		# 		# format.json { respond_with_bip(@face) }
+		# 	end
+		# else
+		# 	render 'edit'
+		# end
+	end
+
 	private
 
 	def face_params
