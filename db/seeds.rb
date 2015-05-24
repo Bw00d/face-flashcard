907e4e38b30213ae8bf76602f33c8eb5b3158155
Brentwood Reid <bwoodreid@gmail.com>
2015-05-24 08:06:55 -0800

    Fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # user = CreateAdminService.new.call
 # puts 'CREATED ADMIN USER: ' << user.email
-user = FirstUser.new.call
+user = CreateFirstUser.new.call
 puts 'Created First User:' << user.email
