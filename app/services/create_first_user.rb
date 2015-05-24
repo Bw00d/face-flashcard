36c3308a84c9fa843955d9b5d114d17258887968
Brentwood Reid <bwoodreid@gmail.com>
2015-05-24 08:03:22 -0800

    Fix typo

--- app/services/create_first_user.rb
+++ app/services/create_first_user.rb
@@ -1,6 +1,6 @@
 class CreateFirstUser
 	def call
-    user = User.create(email: user@example.org, password: 'foobarbaz',
-    									 password_confirmation: 'foobarbaz', name: 'example user')
+    user = User.create(email: 'user@example.org', password: 'foobarbaz',
+    									 password_confirmation: 'foobarbaz', name: 'First User')
   end
 end
