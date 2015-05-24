0f35e6f3391fb6c1c16644d8f01560ad5adba1f9
Brentwood Reid <bwoodreid@gmail.com>
2015-05-23 18:25:53 -0800

    Precompile assets for production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,13 +4,13 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  config.secret_key = ENV["DEVISE_SECRET_KEY"]
+  config.secret_key = ''
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
+  # config.mailer_sender = 'invitation@fire-faces.herokuapp.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
